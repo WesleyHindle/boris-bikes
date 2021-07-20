2601239dba0f83864b9f64a4157c56ad2923fec5
Wesley Hindle <wesleyhindle@gmail.com>
2021-07-20 15:53:39 +0100

    Adding bike instance variable and ability to dock a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,28 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'releases working bikes' do 
     bike = subject.release_bike
     expect(bike).to be_working  
   end 
+
+  it 'docks something' do 
+    bike = Bike.new
+    #We want to return the bike we dock
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  #it { is_expected.to respond_to(:bike) }
 
 end
